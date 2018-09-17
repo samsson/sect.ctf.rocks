81ee924af14189dbad681ef09a904358959ee3db
Lurkkeli <sami.ruohonen16@gmail.com>
2018-09-17 10:35:43 +0200

    Update xorcharacters.py

--- xorcharacters.py
+++ xorcharacters.py
@@ -1,3 +1,5 @@
+#ezdos challenge
+
 import string
 import binascii
 
@@ -5,20 +7,13 @@ lista = [0x66,0x79,0x74, 0x79]
 keys = [0x53,0x48, 0x45, 0x4c]
 val1 = []
 string1 = ""
-def xor3():
+
+def xor():
     for i in range(len(lista)):
-        #print(i)
         val1.append(lista[i] ^ keys[i])
 
-xor3()
+xor()
 for item in val1:
     string1 = string1 + chr(item)
 print("1337-" + string1)
 
-#
-#            if d == i:
-#                print(d)
-#Key = 0x1c 0x33, 0x33, 0x37, 0x53
-#Key = 1 + 0x1e + 0x55 + 0x4e + 0x27
-# 1337-5115
-#
